f7ada7bbb90b9e4ac1c1bba099f96074893fbffa
Ahmed Salem <ahmed.salah98as@gmail.com>
2021-09-22 17:15:19 +0200

    Add Weather API By Ahmed Salem

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,5 +1,7 @@
 function handleSubmit(event) {
     event.preventDefault()
+    const city = "cairo"
+    const key = '26e319dc9f9d7981340126e8fa54f798'
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
@@ -10,6 +12,13 @@ function handleSubmit(event) {
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
+    })
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then(res=>res.json())
+    .then(res=>{console.log(res); return res})
+    .then(res=>{
+    document.getElementById('results').innerHTML += `<p> cairo weather is ${res.weather[0].description}</p>`
+    
     })
 }
 
